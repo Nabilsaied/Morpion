917b460536abeb897886088eb455c0bacec56a76
Nabil <nabil.saied@ecoles-epsi.net>
2024-03-01 08:35:01 +0100

    Morpion jeux

--- morpion.py
+++ morpion.py
@@ -4,8 +4,7 @@ class Morpion():
     def __init__(self):
         self.board = [["-", "-", "-"],
                       ["-", "-", "-"],
-                      ["-", "-", "-"]
-                      ]
+                      ["-", "-", "-"]]
         self.player1 = ""
         self.player2 = "IA"
         self.choice1 = ""
@@ -24,20 +23,19 @@ class Morpion():
 
     def players(self):
         self.player1 = input("Tape ton nom dans le jeu : ")
-        self.choice1 = input("Tu veux jouer avec 'X' ou avec 'O' ? : ").upper()
-        if self.choice1 not in ["X", "O"]:
-            print("Choix invalide. Saisissez 'X' ou 'O'.")
-            return
-        print(self.player1 + ", tu vas jouer avec ", self.choice1)
+        while True:
+            self.choice1 = input("Tu veux jouer avec 'X' ou avec 'O' ? : ").upper()
+            if self.choice1 in ["X", "O"]:
+                break
+            else:
+                print("Choix invalide. Saisissez 'X' ou 'O'.")
+        print(self.player1 + ", tu vas jouer avec " + self.choice1)
         self.choice2 = "O" if self.choice1 == "X" else "X"
         print("L'IA va jouer avec " + self.choice2)
         self.current_player = self.player1
 
     def change_player(self):
-        if self.current_player == self.player1:
-            self.current_player == self.player2
-        else :
-            self.current_player == self.player1
+        self.current_player = self.player2 if self.current_player == self.player1 else self.player1
 
     def check_win(self, choice):
         win_conditions = [
@@ -50,17 +48,25 @@ class Morpion():
             [self.board[0][0], self.board[1][1], self.board[2][2]],
             [self.board[2][0], self.board[1][1], self.board[0][2]],
         ]
-        return [choice, choice, choice] in win_conditions
+        for condition in win_conditions:
+            if condition == [choice, choice, choice]:
+                return True
+        return False
 
+    def check_draw(self):
+        for row in self.board:
+            if "-" in row:
+                return False
+        return True
 
     def jouer(self):
         game_over = False
         self.players()
         while not game_over:
             print(f"C'est le tour de {self.current_player}")
-            if self.current_player == self.player1:
-                valid_move = False
-                while not valid_move:
+            valid_move = False
+            while not valid_move:
+                if self.current_player == self.player1:
                     try:
                         ligne = int(input("Choisis une ligne  (0, 1, 2): "))
                         col = int(input("Choisis une colonne (0, 1, 2): "))
@@ -71,9 +77,7 @@ class Morpion():
                             print("Cette case est déjà prise ou est hors des limites. Essayez de nouveau.")
                     except ValueError:
                         print("S'il vous plaît, entrez un nombre valide.")
-            else:
-                valid_move = False
-                while not valid_move:
+                else:
                     ligne, col = random.randint(0, 2), random.randint(0, 2)
                     if self.board[ligne][col] == "-":
                         self.board[ligne][col] = self.choice2
@@ -87,12 +91,16 @@ class Morpion():
             if self.check_win(self.choice1 if self.current_player == self.player1 else self.choice2):
                 print(f"{self.current_player} a gagné!")
                 game_over = True
-            else:
+            elif self.check_draw():
+                print("Match nul!")
                 game_over = True
+
             self.change_player()
 
-        print("Jeu terminé.")
-
+        if input("Voulez-vous jouer à nouveau ? (oui/non): ").lower() == "oui":
+            self.jouer()
+        else:
+            print("Merci d'avoir joué. Au revoir!")
 
 def main():
     morpion_game = Morpion()
@@ -107,9 +115,6 @@ def main():
         elif choix == '3':
             print("Merci d'avoir joué. Au revoir!")
             break
-        else:
-            print("Choix non valide. Veuillez sélectionner une option valide.")
-
 
 if __name__ == "__main__":
     main()
